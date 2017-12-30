4317cb62639c5b9e28d734dec83180034e4fac17
shusenl <shusenl@sci.utah.edu>
2017-12-29 23:46:08 -0800

    add static folder

--- src/visPackage/visModule.py
+++ src/visPackage/visModule.py
@@ -16,11 +16,12 @@ from flask_socketio import send, emit, socketio, SocketIO, join_room, leave_room
 import webbrowser, threading
 import time
 
+#global app for decorator access
+app = Flask(__name__)
 
 class textEntailVisModule:
     def init(self):
-        self.app = Flask(__name__)
-        self.socketio = SocketIO(self.app)
+        self.socketio = SocketIO(app)
         # pass
         # self.app.run(host='localhost',port=5000,debug=True)
 
@@ -34,13 +35,19 @@ class textEntailVisModule:
 
     # called when the user change the prediction, the attention need to be
     # recomputed by python model
-    def setGradientUpdateHook(self, updateCallback):
+    def setGradientUpdateHook(self, callback):
         pass
 
+    @app.route('/')
+    def index():
+        return app.send_static_file('index.html')
+
+
+    # only call when all setup is done
     def show(self):
-        #delay
+        # delay
         # time.sleep(60)
-        url = 'http://localhost:5000'
+        url = 'http://localhost:5050'
         threading.Timer(1.25, lambda: webbrowser.open(url, new=2) ).start()
-        self.socketio.run(self.app, host='localhost',port=5000, debug=True)
+        self.socketio.run(app, host='localhost',port=5050, debug=True)
         # webbrowser.open('http://localhost:5000', new=2)
