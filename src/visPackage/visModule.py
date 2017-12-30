ea0a6b1a586849f18c3d8f93e4324d289b252898
shusenl <shusenl@sci.utah.edu>
2017-12-29 21:47:25 -0800

    add description

--- src/visPackage/visModule.py
+++ src/visPackage/visModule.py
@@ -3,16 +3,23 @@
  - the portal for sending data to the visualization
  - act as a server for the js based visualization
  - open web browser
+
+
+ Data Model
+ - data object is linked by object id, jason file
+
 '''
 
 from flask import Flask
 from hdanalysis.modules import *
 from hdanalysis.core import *
 
+
 class visModule:
     def init():
         self.app = Flask(__name__)
-        # socketio = SocketIO(self.app)
+        socketio = SocketIO(self.app)
+        socketio.run(app, debug=True)
         # pass
 
     # an input pair ID is used as handle for the correspondence
