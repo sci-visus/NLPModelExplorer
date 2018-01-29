497d36f5f434f4a9c35cbcadd36dda997508275d
Shusen Liu <liu42@llnl.gov>
2018-01-29 13:41:14 -0800

    clean up exampleVis

--- src/exampleVis.py
+++ src/exampleVis.py
@@ -3,15 +3,6 @@ from modelInterface import *
 from sentenceGenerator import *
 
 
-def randomPredict(pair):
-    rnum = np.random.random(3)
-    rnum = rnum/np.sum(rnum)
-    # pred = dict()
-    # pred["neutral"] = rnum[0]
-    # pred["contradiction"] = rnum[1]
-    # pred["entailment"] = rnum[2]
-    return rnum
-
 model = modelInterface(wordDict="../data/snli_1.0.word.dict", wordVec="../data/glove.hdf5", model="local_200_parikh")
 
 
@@ -34,12 +25,8 @@ modelVis = textEntailVisModule(visComponentList)
 modelVis.setPredictionHook(model.predict)
 modelVis.setAttentionHook(model.attention)
 modelVis.setSentencePerturbationHook(gen.perturbSentence)
+# modelVis.predictionChangeHook()
 
-# modelVis.callbackPredictionChange()
 #open browser for visualization
-# modelVis.startServer()
-# model.train()
-# modelVis.closeServer()
-
 modelVis.show()
 # modelVis.startServer()
