d387df86e0b0873d4184267f8ab173a58041d8e7
Zhimin Li <lzm.zhimin@gmail.com>
2018-02-04 15:33:08 -0700

    add dependency tree and color scale to attentionvis

--- src/visPackage/static/components/attentionComponent.js
+++ src/visPackage/static/components/attentionComponent.js
@@ -5,6 +5,7 @@ Matrix representation of attention
 
 */
 
+//beside margin matrix will take 2/3 width and 2/3 height space
 class attentionComponent extends baseComponent {
     constructor(uuid) {
         super(uuid);
@@ -16,29 +17,31 @@ class attentionComponent extends baseComponent {
             bottom: 25,
             left: 25
         };
-
-        this.draw();
+	
+    	
     }
 
     draw() {
         this._updateWidthHeight();
 
-        if (this.data["attention"] !== undefined) {
-            //draw your stuff here
-            //the dimension of the panel is this.width, this.height
-            //the attention is store at this.data["attention"]
-	    //TODO: I need sentence, and dependency tree of the sentence
-            //console.log("attention:", this.data["attention"]);
+        if (this.data["attention"] !== undefined 
+	&& this.data["currentPair"] !== undefined
+	&& this.data['targ_depTree'] !== undefined
+	&& this.data['src_depTree'] !== undefined) {
 	    
 	    //clear all
-	    if(this.svg)
-		    d3.select(this.div).html('');
+    	    if(this.svg)
+	    	d3.select(this.div).html('');
 	    
-	    let pair = this.data['currentPair'];
-	    
-	    //draw attention
+	    //attention matrix
 	    let attMatrix = this.data['attention'];
 	    
+	      	//init svg
+	     this.svg = d3.select(this.div).append("svg")
+	     .attr("width", this.pwidth)
+	     .attr("height", this.pheight)
+	     .append("g")
+	     .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
 	    
 	    //data
     	    this.attList = [];
@@ -46,76 +49,124 @@ class attentionComponent extends baseComponent {
 		    for(let j = 0; j < attMatrix[i].length; j++){
 		    	this.attList.push(attMatrix[i][j])
 		    }
-		    
-	    this.srcWords = pair[0].match(/\S+/g);
-	    this.srcWords = ["\<s\>"].concat(this.srcWords);
-	    this.targWords = pair[1].match(/\S+/g);
-	    this.targWords = ["\<s\>"].concat(this.targWords);    
-		    
-            
-            //sentence position
-            this.computeWordPosition(this.srcWords, this.targWords);
 	    
-	    //init svg
-	    this.svg = d3.select(this.div).append("svg")
-            .attr("width", this.pwidth)
-            .attr("height", this.pheight)
-            .append("g")
-            .attr("transform", "translate(" + this.margin.left + "," +
-            	this.margin.top + ")");
+	    //location of words
+            this.computeWordPosition(this.srcWords, this.targWords);
+	
+	    //matrix 
+	    let rectw = (this.width * 3/4) / this.targWords.length,
+		recth = (this.height * 3/4) / this.srcWords.length;
 		
-	    //Draw src text
-	    this.svg.selectAll('.attentionComponent_srcwords')
-	    	.data(this.srcWords)
-		.enter()
-		.append('text')
-		.text(d => d)
-		.attr('x', (d, i) => this.srcPos[i])
-		.attr("y", this.margin.top)
-		.style("font-size", 10)
-                .style("alignment-baseline", "middle")
-                .style("text-anchor", "middle");
+	    this.svg.selectAll('.attentionComponent_matrix_rect').data(this.attList)
+	    .enter()
+	    .append('rect')
+	    .attr('class', 'attentionComponent_matrix_rect')
+	    .attr('x', (d,i)=>{
+		return this.width * 1/4 + (i % this.targWords.length) * rectw;
+	    })
+	    .attr('y', (d,i)=>{
+		return this.height * 1/4 + Math.floor(i / this.targWords.length) * recth;
+	    })
+	    .attr('width', rectw)
+	    .attr('height', recth)
+	    .style('stroke', 'black')
+	    .style('stroke-width', '1px')
+	    .style('fill', d=>{return d3.interpolateRdBu(1-d);});
 		
 	    //Draw targ text
-	    this.svg.selectAll('.attentionCompoentn_targwords')
-		.data(this.targWords)
-		.enter()
-		.append('text')
-		.text(d=>d)
-		.attr('x', this.margin.left)
-		.attr("y", (d, i) => this.targPos[i])
-		.style("alignment-baseline", "middle")
-		.style("font-size", 10)
-                .style("text-anchor", "middle");
-		
-	
-	
-	
-	    //sentence position
+	    //text
+	    this.targ_dep = new dependencyTreePlot(this.svg, 'h-top',this.targPos, this.data['targ_depTree'], this.width, this.height);
+	    this.svg.selectAll('.attentionComponent_targWords')
+    	    .data(this.targWords)
+	    .enter()
+	    .append('text')
+	    .text(d => d)
+	    .attr('x', (d, i) => this.targPos[i].x)
+	    .attr("y", (d, i) => this.targPos[i].y)
+    	    .attr("transform", (d, i) => {
+		return "rotate(-45, "+this.targPos[i].x+' , '+this.targPos[i].y+')';
+	    })
+            .style("font-size", 10)
+            .style("alignment-baseline", "middle")
+            .style("text-anchor", "middle");
 	    
+		
+	    //Draw src text
+	    this.src_dep = new dependencyTreePlot(this.svg, 'v-left', this.srcPos, this.data['src_depTree'], this.width, this.height);
+	    this.svg.selectAll('.attentionCompoentn_srcWords')
+	    .data(this.srcWords)
+	    .enter()
+	    .append('text')
+            .text(d=>d)
+	    .attr('x', (d, i) => this.srcPos[i].x)
+	    .attr("y", (d, i) => this.srcPos[i].y)
+	    .style("alignment-baseline", "middle")
+	    .style("font-size", 10)
+            .style("text-anchor", "middle");
+	    
+	    //Draw color scale bar
+	    let linearGradient = this.svg.append('defs').append('linearGradient')
+    	    .attr('id', 'attention_heatmap_gradient')
+    	    .attr('x1', '0%')
+    	    .attr('y1', '0%')
+    	    .attr('x2', '100%')
+    	    .attr('y2', '0%');
 	
+    	    //color gradient break point
+    	    for(let i = 0; i <= 10; i++){
+    		linearGradient.append('stop')
+    		.attr('offset', (i * 10) + '%')
+    		.attr('stop-color', d3.interpolateRdBu(i/10));
+    	    }
+	
+    	    linearGradient.append('stop')
+    	    .attr('offset', '100%')
+    	    .attr('stop-color', d3.interpolateRdBu(1));
+	
+    	    //render color scale map
+    	    this.svg.append('rect')
+    	    .attr('x', this.margin.left)
+    	    .attr('y', this.margin.top)
+    	    .attr('width', this.width * 1/6)
+    	    .attr('height', this.height /10)
+    	    .style('fill', 'url(#attention_heatmap_gradient)');
+	    
+	    //color scale ticks
+	    let ticks = [1,0.5,0];
+	    
+	    let ticksdata = ticks.map((d, i)=>{
+	    	return {
+			    'text':d, 
+		    	    'x': this.margin.left + i * this.width/12,
+			    'y': this.margin.top
+		    };
+	    });
+	    
+	    this.svg.selectAll('.attention_heatmap_gradient_ticks')
+	    .data(ticksdata)
+	    .enter()
+	    .append('text')
+	    .attr('x', d => d.x)
+	    .attr('y', d => d.y)
+	    .text(d => d.text)
+	    .style("alignment-baseline", "middle")
+	    .style("font-size", 12)
+            .style("text-anchor", "middle");
         }
     }
-	
-    draw_attention(matrx, row, col){
-    	
-    }
     
-    draw_dep_tree(sen, sen_dep, horv){
-    	
-    }
-
-    computeWordPosition(src, targ) {
+    
+    
+    computeWordPosition(src, targ) {  
         // console.log(src, targ);
-        this.srcPos = src.map((d, i) => {
-            return this.width / (src.length + 1) * (i + 2)
+        this.srcPos = this.srcWords.map((d, i) => {
+		return {'y':(this.height  * 0.75) / this.srcWords.length * (i + 0.5) + this.height / 4, 'x':this.width * 1/4 - this.margin.left};
         });
 	
-        this.targPos = targ.map((d, i) => {
-		return this.height / (targ.length + 1) * (i + 0.5) + this.margin.top;
+        this.targPos = this.targWords.map((d, i) => {
+		return {'x':(this.width  * 0.75) / this.targWords.length * (i + 0.5)+ this.width / 4, 'y':this.height * 1/4 - this.margin.top};
         });
     }
-
 
     resize() {
 
@@ -130,6 +181,45 @@ class attentionComponent extends baseComponent {
                 //if attention is updated, redraw attention
                 this.draw();
                 break;
+            case "currentPair":
+            	let pair = msg["data"]["data"];
+		
+		this.srcWords = ["\<s\>"].concat(pair[0].match(/\S+/g));
+    	    	this.targWords = ["\<s\>"].concat(pair[1].match(/\S+/g)); 
+		
+            	//parse the sentence
+            	this.callFunc("parseSentence", {
+            	         "sentence": pair[0]
+            	});
+            	this.callFunc("parseSentence", {
+            	  	 "sentence": pair[1]
+            	});
+            	break;
         }
     }
+    
+    parseFunctionReturn(msg) {
+        switch (msg["func"]) {
+            case "parseSentence":
+                this.handleParsedSentence(msg["data"]);
+        }
+    }
+    
+    /////////////// handler /////////////////
+    handleParsedSentence(parseResult) {
+        console.log(parseResult);
+	
+        if (parseResult.data["sentence"] == this.data["currentPair"][0]) {
+		this.data['src_depTree'] = parseResult.data.depTree;
+	}
+	else if(parseResult.data["sentence"] == this.data["currentPair"][1]){
+		this.data['targ_depTree'] = parseResult.data.depTree;
+	}
+	else{
+		throw Error('unknow sentence in attention Component handle parsedSentence');
+	}
+	
+	this.draw();
+    }
+    
 }
--- src/visPackage/static/plot/dependencyTreePlot.js
+++ src/visPackage/static/plot/dependencyTreePlot.js
@@ -4,255 +4,204 @@ add dependencyTreePlot to an existing svg
 */
 
 class dependencyTreePlot {
-    constructor(svg, orientation, pos) {
+	
+        //pos: The index position
+	//width: The width of sentence
+	//heigth: The height of sentence 
+    constructor(svg, orientation, pos, dep_triples, width, height) {
         this.svg = svg;
         this.orientation = orientation;
-
+	this.pos = pos;
+	this.dep_triples = dep_triples;
+	this.width = width;
+	this.heigth = height;
+	
+	//text box width and height
+	this.text_box_width = 15;
+	this.text_box_height = 10;
+	
+	this.draw();
     }
 
     setCollapseHandle(func) {
         this.callback = func;
-
     }
 
     onHandleCollapse() {
         this.callback(this.sentenceMask); //[1,0,0,1]
-
     }
 
+    //draw the dependency tree over sentence
     draw() {
+    	
+	//arrow
+        this.svg.append("svg:defs")
+        .append("svg:marker")
+	.attr("id", "arrow")
+	.attr('viewBox', '0 0 10 10')
+        .attr("refX", 1)
+        .attr("refY", 5)
+        .attr("markerWidth", 6)
+        .attr("markerHeight", 6)
+        .attr("orient", "auto")
+        .append("svg:path")
+        .attr("d", "M 0 0 L 10 5 L 0 10 z")
+        .style('fill', 'steelblue');
 
-    }
-
-    draw_dep_tree(x, y, sen, sen_dep_tree, horv) {
-        canvas = this.svg;
-
-        //location of text
-        let text_loc = {};
-
-        for (let i = 0; i < sen.length; i++) {
-            if (horv == 'h') {
-                text_loc[sen[i] + i] = {
-                    'x': x + i * rectw,
-                    'y': y - 10
-                };
-            } else if (horv == 'v') {
-                text_loc[sen[i] + i] = {
-                    'x': x - rectw,
-                    'y': y + recth * i
-                };
-            } else {
-                throw Error('unknow confi. in draw_dep_tree');
-            }
-        }
-
-        //text
-        canvas.selectAll('.dep_tree_word').data(sen).enter()
-            .append('text')
-            .text(function(d) {
-                return d;
-            })
-            .attr('x', function(d, i) {
-                if (horv == 'h')
-                    return text_loc[d + i].x;
-                else
-                    return text_loc[d + i].x + rectw;
-            })
-            .attr('y', function(d, i) {
-                if (horv == 'h')
-                    return text_loc[d + i].y + 10;
-                else
-                    return text_loc[d + i].y;
-            })
-            .attr('text-anchor', 'middle')
-            .attr('dominant-baseline', 'central')
-            .style('font-size', 12)
-            .on('mouseover', function(d, i) {
-                d3.selectAll('.' + d + '_' + i + '_path').style(
-                    "stroke-dasharray", "").style('stroke',
-                    'orange');
-                d3.selectAll('.' + d + '_' + i + '_rect').style(
-                    'stroke', 'orange');
-            })
-            .on('mouseout', function(d, i) {
-                d3.selectAll('.' + d + '_' + i + '_path').style(
-                    "stroke-dasharray", "4,4").style('stroke',
-                    'steelblue');
-                d3.selectAll('.' + d + '_' + i + '_rect').style(
-                    'stroke', 'gray');
-            })
-            .on('click', function(d, i) {
-                if (horv == 'h') {
-
-                } else {
-
-                }
-            });
-
-        //path
-        canvas.append("svg:defs")
-            .append("svg:marker")
-            .attr("id", "arrow")
-            .attr('viewBox', '0 0 10 10')
-            .attr("refX", 1)
-            .attr("refY", 5)
-            .attr("markerWidth", 6)
-            .attr("markerHeight", 6)
-            .attr("orient", "auto")
-            .append("svg:path")
-            .attr("d", "M 0 0 L 10 5 L 0 10 z")
-            .style('fill', 'steelblue');
-
+	//line function
         let lineFunction = d3.line()
-            .x(function(d) {
+        .x(function(d) {
                 return d.x;
-            })
-            .y(function(d) {
+        })
+    	.y(function(d) {
                 return d.y;
-            })
-            .curve(d3.curveLinear);
+        })
+    	.curve(d3.curveLinear);
 
-        canvas.selectAll('.dep_tree_dep_path').data(sen_dep_tree).enter()
-            .append('path')
-            .attr('d', function(d, i) {
-                let data = [],
-                    word1 = sen[d[0]] + d[0],
-                    word2 = sen[d[2]] + d[2];
-
-                if (horv == 'h') {
-                    //first point
-                    data.push(text_loc[word1]);
-                    //second point
-                    data.push({
-                        'x': text_loc[word1].x * 5 / 6 +
-                            text_loc[word2].x * 1 / 6,
-                        'y': text_loc[word1].y - Math.abs(
-                            text_loc[word1].x - text_loc[
-                                word2].x) / rectw * 15
-                    });
-                    //third point
-                    data.push({
-                        'x': text_loc[word1].x * 1 / 6 +
-                            text_loc[word2].x * 5 / 6,
-                        'y': text_loc[word1].y - Math.abs(
-                            text_loc[word1].x - text_loc[
-                                word2].x) / rectw * 15
-                    });
-                    //third point
-                    data.push(text_loc[word2]);
-                } else {
-                    //first point
-                    data.push(text_loc[word1]);
-                    //second point
-                    data.push({
-                        'x': text_loc[word1].x - Math.abs(
-                            text_loc[word1].y - text_loc[
-                                word2].y) / recth * 20,
-                        'y': text_loc[word1].y * 5 / 6 +
-                            text_loc[word2].y * 1 / 6
-                    });
-                    //third point
-                    data.push({
-                        'x': text_loc[word1].x - Math.abs(
-                            text_loc[word1].y - text_loc[
-                                word2].y) / recth * 20,
-                        'y': text_loc[word1].y * 1 / 6 +
-                            text_loc[word2].y * 5 / 6
-                    });
-                    //fourth point
-                    data.push(text_loc[word2]);
-                }
-                return lineFunction(data);
-            })
-            .attr('class', function(d, i) {
-                let word = sen[d[0]];
-                return word + '_' + d[0] + '_path';
-            })
-            .attr("fill", "none")
-            .attr("stroke", "steelblue")
-            .attr("stroke-linejoin", "round")
-            .attr("stroke-linecap", "round")
-            .attr("stroke-width", 1.5)
-            .style("stroke-dasharray", "4,4")
-            .style("marker-end", "url(#arrow)");
+	//path
+	let path_loc = this.pathLocation();
+        this.svg.selectAll('.dep_tree_dep_path').data(path_loc)
+	.enter()
+        .append('path')
+        .attr('d', function(d){
+		return lineFunction(d);
+	})
+    	.attr("fill", "none")
+    	.attr("stroke", "gray")
+	.attr("stroke-opacity", 0.5)
+    	.attr("stroke-linejoin", "round")
+    	.attr("stroke-linecap", "round")
+    	.attr("stroke-width", 1.5)
+    	.style("stroke-dasharray", "4,4")
+    	.style("marker-end", "url(#arrow)");
 
         //component rect
-        canvas.selectAll('.dep_tree_rel_text').data(sen_dep_tree).enter()
-            .append('rect')
-            .attr('width', rectw / 2)
-            .attr('height', recth / 2)
-            .attr('rx', 2)
-            .attr('ry', 2)
-            .attr('x', function(d, i) {
-                let word1 = sen[d[0]] + d[0],
-                    word2 = sen[d[2]] + d[2];
-
-                if (horv == 'h') {
-                    return (text_loc[word1].x + text_loc[word2].x) / 2 -
-                        rectw / 4;
-                } else {
-                    return text_loc[word1].x - Math.abs(text_loc[word1]
-                            .y - text_loc[word2].y) / recth * 20 -
-                        rectw / 4
-                }
-            })
-            .attr('y', function(d, i) {
-                let word1 = sen[d[0]] + d[0],
-                    word2 = sen[d[2]] + d[2];
-                if (horv == 'h')
-                    return text_loc[word1].y - Math.abs(text_loc[word1]
-                            .x - text_loc[word2].x) / rectw * 15 -
-                        recth / 4;
-                else
-                    return (text_loc[word1].y + text_loc[word2].y) / 2 -
-                        recth / 4;
-            })
-            .attr('class', function(d, i) {
-                let word = sen[d[0]];
-                return word + '_' + d[0] + '_rect';
-            })
-            .attr('font-size', 8)
-            .attr('fill', 'white')
-            .style('stroke', 'gray')
-            .style('stroke-width', '1px');
-
-
+	let text_loc = this.textLocation();
+	
+	//text background rect
+        /*this.svg.selectAll('.dep_tree_rel_rect').data(text_loc).enter()
+    	.append('rect')
+    	.attr('width', this.text_box_width)
+    	.attr('height', this.text_box_height)
+    	.attr('rx', 2)
+    	.attr('ry', 2)
+    	.attr('x', function(d, i) {
+        	return d.x;
+    	})
+    	.attr('y', function(d, i) {
+        	return d.y;
+    	})
+    	.attr('class', function(d, i) {
+                //let word = sen[d[0]];
+                //return word + '_' + d[0] + '_rect';
+    	})
+    	.attr('font-size', 8)
+    	.attr('fill', 'white')
+    	.style('stroke', 'gray')
+    	.style('stroke-width', '1px');*/
+	
         //component text
-        canvas.selectAll('.dep_tree_rel_text').data(sen_dep_tree).enter()
-            .append('text')
-            .text(function(d) {
-                return d[1]
-            })
-            .attr('width', rectw / 2)
-            .attr('height', recth / 2)
-            .attr('x', function(d, i) {
-                let word1 = sen[d[0]] + d[0],
-                    word2 = sen[d[2]] + d[2];
+        this.svg.selectAll('.dep_tree_rel_text').data(text_loc)
+	.enter()
+    	.append('text')
+    	.text(function(d) {
+                return d.text;
+    	})
+    	.attr('x', function(d, i) {
+        	return d.x;
+    	})
+    	.attr('y', function(d, i) {
+                return d.y;
+    	})
+    	.attr('font-size', 8)
+    	.attr("text-anchor", "middle")
+    	.attr("dominant-baseline", "central");
+	
+	
 
-                if (horv == 'h') {
-                    return (text_loc[word1].x + text_loc[word2].x) / 2;
-                } else {
-                    return text_loc[word1].x - Math.abs(text_loc[word1]
-                        .y - text_loc[word2].y) / recth * 20
-                }
-            })
-            .attr('y', function(d, i) {
-                let word1 = sen[d[0]] + d[0],
-                    word2 = sen[d[2]] + d[2];
-                if (horv == 'h')
-                    return text_loc[word1].y - Math.abs(text_loc[word1]
-                        .x - text_loc[word2].x) / rectw * 15;
-                else
-                    return (text_loc[word1].y + text_loc[word2].y) / 2;
-            })
-            .attr('class', function(d, i) {
-                let word = sen[d[0]];
-                return word + '_' + d[0] + '_text';
-            })
-            .attr('font-size', 10)
-            .attr("text-anchor", "middle")
-            .attr("dominant-baseline", "central");
+        
+    }
+    
+    textLocation(){
+	let data = [];
+    	for(let i = 0; i < this.dep_triples.length; i++){
+		let d = this.dep_triples[i],
+		word1_loc = this.pos[d[0]],
+	        word2_loc = this.pos[d[2]],
+		item = {'text':d[1]};
 
+		 
+	        if (this.orientation == 'h-top') {
+			item['x'] = (word1_loc.x + word2_loc.x) / 2;
+			item['y'] = word1_loc.y - Math.abs(d[0] - d[2]) * this.text_box_height - this.text_box_height * 1.5;
+	        } 
+		else if(this.orientation == 'v-left') {
+			item['x'] = word1_loc.x - Math.abs(d[0] - d[2]) * this.text_box_width - this.text_box_width * 1.5;
+	        	item['y'] = (word1_loc.y + word2_loc.y) / 2;
+		}  
+		data.push(item)
+	}
+	return data;
     }
 
+    pathLocation(){
+	    let data = [];
+	    
+	    for(let i = 0; i < this.dep_triples.length; i++){
+		let d = this.dep_triples[i],
+		word1_loc = this.pos[d[0]],
+        	word2_loc = this.pos[d[2]],
+		item = [];
+
+           	if (this.orientation == 'h-top') {
+                	//first point
+                	item.push({
+                		'x':word1_loc.x,
+				'y':word1_loc.y - this.text_box_height * 1.5
+                	});
+                	//second point
+                	item.push({
+                		'x': word1_loc.x * 5 / 6 + word2_loc.x * 1 / 6,
+				'y': word1_loc.y - Math.abs(d[0]-d[2]) * this.text_box_height - this.text_box_height * 1.5
+               	 	});
+                	//third point
+               	 	item.push({
+               		 	'x': word1_loc.x * 1 / 6 + word2_loc.x * 5 / 6,
+                    	        'y': word1_loc.y - Math.abs(d[0]-d[2]) * this.text_box_height - this.text_box_height * 1.5
+                	});
+                	//fourth point
+               	 	item.push({
+               	 		'x':word2_loc.x,
+				'y':word2_loc.y - this.text_box_height * 1.5
+               	 	});
+       	   	}
+		else if(this.orientation == 'v-left') {
+                	//first point
+                	item.push({
+                		'x':word1_loc.x - this.text_box_width * 1.5,
+				'y':word1_loc.y
+                	});
+                	//second point
+                	item.push({
+                		'x': word1_loc.x - Math.abs(d[0] - d[2]) * this.text_box_width - this.text_box_width * 1.5,
+               		 	'y': word1_loc.y * 5 / 6 + word2_loc.y * 1 / 6
+               	 	});
+                	//third point
+                	item.push({
+                    	        'x': word1_loc.x - Math.abs(d[0] - d[2]) * this.text_box_width - this.text_box_width * 1.5, 
+                    	        'y': word1_loc.y * 1 / 6 + word2_loc.y * 5 / 6
+                	});
+                	//fourth point
+                	item.push({
+                		'x':word2_loc.x - this.text_box_width * 1.5,
+				'y':word2_loc.y
+                	});
+            	}
+		else
+			throw Error('unknow orientation');
+		
+		data.push(item);
+	    }
+	    return data;
+    }
 }
