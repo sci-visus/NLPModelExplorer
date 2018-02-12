2e391a4f9c5d240daf88db49a16690519ebff5b2
Shusen Liu <liu42@llnl.gov>
2018-02-12 15:03:22 -0800

    add camera icon

--- src/visPackage/static/lib/svgExporter.js
+++ src/visPackage/static/lib/svgExporter.js
@@ -10,16 +10,17 @@ class svgExporter {
         //   <i class="fas fa-camera-retro"></i>
         // </div>
         this.svg.append('text')
+            .attr("id", "cameraIcon")
             .attr("x", this.pos[0])
             .attr("y", this.pos[1] + 20)
             .attr('font-family', 'FontAwesome')
-            .attr("color", "grey")
             .attr('font-size', function(d) {
-                return 3 + 'em'
+                return 2 + 'em'
             })
             .text(function(d) {
                 return '\uf030'
             })
+            .attr("fill", "lightgrey")
             .style("alignment-baseline", "middle");
     }
 
