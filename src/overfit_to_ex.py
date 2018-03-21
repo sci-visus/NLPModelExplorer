c300f0497d5e72a068bdeef268b5218a47b25777
Tao Li <taoli@taos-mbp.lan>
2018-03-20 21:01:26 -0600

    added mira

--- src/overfit_to_ex.py
+++ src/overfit_to_ex.py
@@ -20,7 +20,6 @@ from embeddings import *
 
 
 parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-print type(parser)
 # parser.add_argument('--res', help="Path to resource files, separated by comma.", default="")
 parser.add_argument('--load_file', help="Path from where model to be loaded.", default="model")
 
@@ -37,6 +36,7 @@ parser.add_argument('--word_vecs', help="The path to word embeddings", default =
 parser.add_argument('--fix_word_vecs', help="Whether to make word embeddings NOT learnable", type=int, default=1)
 parser.add_argument('--seed', help="The random seed", type=int, default=3435)
 parser.add_argument('--gpuid', help="The GPU index, if -1 then use CPU", type=int, default=-1)
+parser.add_argument('--mira_c', help="The constant weight in MIRA loss function", type=float, default=1.0)
 parser.add_argument('--customize_att', help="Whether to use customized att values instead of computed ones", type=int, default=0)
 parser.add_argument('--zero_out_encoder', help="Whether to zero out the gradient of encoder layers before update", type=int, default=0)
 parser.add_argument('--zero_out_attention', help="Whether to zero out the gradient of attention layers before update", type=int, default=0)
@@ -44,6 +44,114 @@ parser.add_argument('--zero_out_classifier', help="Whether to zero out the gradi
 
 def pick_label(dist):
 	return np.argmax(dist, axis=1)
+
+def is_param_enabled(opt, param_name):
+	if opt.zero_out_encoder == 1 and 'encoder' in param_name:
+		return False
+	elif opt.zero_out_attention == 1 and 'attention' in param_name:
+		return False
+	elif opt.zero_out_classifier == 1 and 'classifier' in param_name:
+		return False
+	return True
+
+
+# the mira loss is |w' - w|^2 + C * J(w')
+# where J is the original network loss on w'
+# and w' is the target weights to get.
+# to mimic mira loss, we use:
+#	network weights to train is w',
+#	w_start as w and it will be taken as constant,
+# NOTE, w_start should be different from current network weights
+# NOTE, w_start is a map of torch tensors
+def build_mira_loss(opt, m, w_start, nn_loss):
+	zero = Variable(torch.zeros(1), requires_grad=False)
+	if opt.gpuid != -1:
+		zero = zero.cuda()
+
+	l2_reg = zero
+	# accumulate to l2_reg
+	for name, w_prime in m.named_parameters():
+		# check if this parameter participate in mira pipeline
+		if w_prime.requires_grad and is_param_enabled(opt, name):
+			w = Variable(w_start[name], requires_grad=False)
+			l2_reg += torch.sqrt(((w_prime - w) * (w_prime - w)).sum())
+	#
+	c = Variable(torch.ones(1)*opt.mira_c, requires_grad=False)
+	return l2_reg + c * nn_loss
+
+
+def clone_cur_weights(m):
+	w = {}
+	for name, p in m.named_parameters():
+		w[name] = p.data.clone()
+	return w
+
+
+def get_weight_offset(m, w_start):
+	offset = {}
+	for name, p in m.named_parameters():
+		offset[name] = p.data - w_start[name]
+	return offset
+
+
+# runs multiple passes of learning until fit to the right prediction
+def mirafit_to_ex(opt, shared, wv, optim, m, ex, w_start):
+	m.train(True)
+	if opt.dropout != 0.0:
+		print('dropout should be 0 during overfitting.')
+
+	total_loss = 0.0
+	num_sents = 0
+	num_correct = 0
+	# loss function
+	criterion = torch.nn.NLLLoss(size_average=False)
+	if opt.gpuid != -1:
+		criterion = criterion.cuda()
+
+	# number of batches
+	source, target, batch_ex_idx, batch_l, source_l, target_l, y_gold = ex
+	wv_idx1 = Variable(source, requires_grad=False)
+	wv_idx2 = Variable(target, requires_grad=False)
+	y_gold = Variable(y_gold, requires_grad=False)
+
+	# lookup word vecs
+	word_vecs1 = wv(wv_idx1)
+	word_vecs2 = wv(wv_idx2)
+
+	# update network parameters
+	m.update_context(batch_ex_idx, batch_l, source_l, target_l)
+
+	# forward pass
+	y_dist = m.forward(word_vecs1, word_vecs2)
+	nn_loss = criterion(y_dist, y_gold)
+	mira_loss = build_mira_loss(opt, m, w_start, nn_loss)
+
+	# register hooks
+	# 	the variable name must be recorded in shared holder
+	#	then after backward pass, the gradient of the specified variable name will be recorded in shared
+	# e.g. retain_grad(shared, 'att_soft1')
+	#	then after backward pass, shared.att_soft1.grad will has its gradient
+	retain_grad(shared, 'att_soft1')
+
+	# zero out previous gradient and do backward pass
+	m.zero_grad()
+	mira_loss.backward()
+
+	# zero out some gradients before update
+	if opt.zero_out_encoder == 1:
+		m.encoder.zero_grad()
+	if opt.zero_out_attention == 1:
+		m.attention.zero_grad()
+	if opt.zero_out_classifier == 1:
+		m.classifier.zero_grad()
+
+	# update
+	optim.step(shared)
+	total_loss = mira_loss.data[0]
+	# print "total_loss", total_loss
+	y_dist = m.forward(word_vecs1, word_vecs2)
+	# return the updated model, and y prediction (probabilities)
+	return m, y_dist.data.exp()
 
 # This function will run one epoch of training given a batch of examples
 # 	updates will be made to model m
@@ -92,7 +200,7 @@ def overfit_to_ex(opt, shared, wv, optim, m, ex):
 	# zero out previous gradient and do backward pass
 	m.zero_grad()
 	loss.backward()
-	print loss
+	#print loss
 
 	# zero out some gradients before update
 	if opt.zero_out_encoder == 1:
@@ -157,12 +265,27 @@ def main(args):
 	y_gold = torch.LongTensor([0])
 	ex = (source, target, batch_ex_idx, batch_l, sent_l1, sent_l2, y_gold)
 
-	m, y = overfit_to_ex(opt, shared, embeddings, optim, pipeline, ex)
-	print(y)
-	# print(shared.att_soft1.grad)
-	# m, y = overfit_to_ex(opt, shared, embeddings, optim, pipeline, ex)
-	# print(y)
-	# print(shared.att_soft1.grad)
+	# the mira loss is |w' - w|^2 + C * J(w')
+	# where J is the original network loss on w'
+	# and w' is the target weights to get.
+
+	# record the current network weights and keep it as constant
+	w_start = clone_cur_weights(pipeline)
+
+	# get a initial w'
+	# 	here, it's gonna be updated w using sgd
+	# just one pass sgd
+	pipeline, y = overfit_to_ex(opt, shared, embeddings, optim, pipeline, ex)
+
+	# then perform mira fit
+	# just one pass sgd, run multiple if need to overfit
+	for i in xrange(10):
+		print('epoch {0}'.format(i))
+		pipeline, y = mirafit_to_ex(opt, shared, embeddings, optim, pipeline, ex, w_start)
+		print(y)
+
+	# get weight offset
+	offset = get_weight_offset(pipeline, w_start)
 
 
 
