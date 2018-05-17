d2c36f69e4837d8c83c2bafb38c660f99696a060
shusenl <shusenl@sci.utah.edu>
2018-05-16 23:23:21 -0700

    paragraph view is working

--- src/visPackage/MCModule.py
+++ src/visPackage/MCModule.py
@@ -15,10 +15,10 @@ data organization structure
     - predictionsHighlight (use the index to update currentPair display)
 '''
 
-QAexample = [
+exampleData = [
 {
     "index": 0,
-    "src":"A reusable launch system (RLS, or reusable launch vehicle, RLV) is a launch system which is capable of launching a payload into space more than once. This contrasts with expendable launch systems, where each launch vehicle is launched once and then discarded. No completely reusable orbital launch system has ever been created. Two partially reusable launch systems were developed, the Space Shuttle and Falcon 9. The Space Shuttle was partially reusable: the orbiter (which included the Space Shuttle main engines and the Orbital Maneuvering System engines), and the two solid rocket boosters were reused after several months of refitting work for each launch. The external tank was discarded after each flight.",
+    "src":"<s> A reusable launch system (RLS, or reusable launch vehicle, RLV) is a launch system which is capable of launching a payload into space more than once. This contrasts with expendable launch systems, where each launch vehicle is launched once and then discarded. No completely reusable orbital launch system has ever been created. Two partially reusable launch systems were developed, the Space Shuttle and Falcon 9. The Space Shuttle was partially reusable: the orbiter (which included the Space Shuttle main engines and the Orbital Maneuvering System engines), and the two solid rocket boosters were reused after several months of refitting work for each launch. The external tank was discarded after each flight. \n",
     "targ":"How many partially reusable launch systems were developed?",
     "pred": "Two"
 }
@@ -39,9 +39,9 @@ class MCModule(visModule):
     def index():
         dataManager.clear()
         dataManager.setData("componentLayout", layoutConfig)
-        dataManager.setData("sentenceList", QAexample)
+        dataManager.setData("sentenceList", exampleData)
         # dataManager.setData("pipeline", pipelineState)
-        # dataManager.setData("currentPair", {"sentences":[exampleData[0]['src'], exampleData[0]['targ']],"label":exampleData[0]['pred']})
+        dataManager.setData("currentPair", {"sentences":[exampleData[0]['src'], exampleData[0]['targ']],"label":exampleData[0]['pred']})
         return app.send_static_file('index.html')
 
     def setSentencePerturbationHook(self, callback):
--- src/visPackage/static/components/paragraphComponent.js
+++ src/visPackage/static/components/paragraphComponent.js
@@ -1,15 +1,35 @@
-class paragraphComponenet extends baseComponent {
+class paragraphComponenet extends sentenceComponent {
     constructor(uuid) {
         super(uuid);
+        // this.subscribeDatabyNames(["sentenceList", "currentPair"]);
 
     }
 
     parseDataUpdate(msg) {
-        //dynamicSourceHighlight
+        super.parseDataUpdate(msg);
+        switch (msg['name']) {
+            case "dynamicSourceHighlight":
+                break;
+        }
     }
 
-    draw() {
-        //sync highlight with attention
-
-    }
+    // draw() {
+    //     //sync highlight with attention
+    //
+    // }
 }
+
+// onReceiveSentenceList() {
+//     // console.log("sentenceList:", this.data["sentenceList"]);
+//     d3.select(this.div + "selectExample")
+//         .on("change", this.onChangeOriginalPair.bind(this));
+//     var options = d3.select(this.div + "selectExample")
+//         .selectAll('option')
+//         .data(this.data["sentenceList"]).enter()
+//         .append('option')
+//         .text(function(d) {
+//             return d["src"].substring(4) + " | " + d["targ"].substring(
+//                 4);
+//         })
+//         .property("value", (d, i) => i);
+// }
